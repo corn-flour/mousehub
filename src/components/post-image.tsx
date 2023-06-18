c8cabfe33f07d08f951597804ef6ba1b2cd7ce1f
corn-flour <26082352+corn-flour@users.noreply.github.com>
2023-06-18 11:58:46 -0700

    remove unnecessary code

--- src/components/post-image.tsx
+++ src/components/post-image.tsx
@@ -1,25 +1,8 @@
 import Image from "next/image"
 
 // renders an image of the post
-// blur the image if it is nsfw
-export const PostImage = (props: {
-    imageURL: string
-    nsfw?: boolean
-    alt: string
-}) => {
-    const { imageURL, nsfw, alt } = props
-
-    if (nsfw) {
-        return (
-            <Image
-                src={imageURL}
-                alt={alt}
-                className="max-h-[600px] rounded-lg object-cover object-top transition-all"
-                width={672}
-                height={672}
-            />
-        )
-    }
+export const PostImage = (props: { imageURL: string; alt: string }) => {
+    const { imageURL, alt } = props
 
     return (
         <Image
