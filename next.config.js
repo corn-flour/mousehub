a97a6698b36c86d4f8664384d20a23dfb1904ec0
corn-flour <26082352+corn-flour@users.noreply.github.com>
2023-06-27 19:21:53 -0700

    modularize lucide-icons?

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
     experimental: {
         serverActions: true,
     },
+    modularizeImports: {
+        // apparently this might help with dev server perf?
+        // https://twitter.com/dobroslav_dev/status/1673361264985161733
+        "lucide-react": {
+            transform:
+                "lucide-react/dist/esm/icons/{{lowerCase kebabCase member}}",
+            skipDefaultConversion: true,
+        },
+    },
 }
 
 module.exports = nextConfig
