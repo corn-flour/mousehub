7b2c6b9f88a3be00b522fa0793df85cf210467c4
corn-flour <26082352+corn-flour@users.noreply.github.com>
2023-07-03 17:05:55 -0700

    update style for comment form

--- src/components/markdown/markdown.config.tsx
+++ src/components/markdown/markdown.config.tsx
@@ -149,7 +149,7 @@ export const components: Partial<
     code: ({ className, node, ...props }) => (
         <code
             className={cn(
-                "relative rounded border px-[0.3rem] py-[0.2rem] font-mono text-sm",
+                "relative rounded px-[0.3rem] py-[0.2rem] font-mono text-sm",
                 className,
             )}
             {...props}
