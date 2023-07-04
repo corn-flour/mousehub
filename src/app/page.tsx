19daa26fc5185d392b806862a54cd70e0a3ab72d
corn-flour <26082352+corn-flour@users.noreply.github.com>
2023-07-03 22:49:56 -0700

    naive homepage redirect logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            TODO:
-        </main>
-    )
+import { getServerSession } from "next-auth"
+import { authOptions } from "./api/auth/[...nextauth]/route"
+import { redirect } from "next/navigation"
+
+export default async function Home() {
+    const session = await getServerSession(authOptions)
+    if (session) {
+        redirect(`/${session.instanceURL}`)
+    } else {
+        redirect(`/login`)
+    }
 }
